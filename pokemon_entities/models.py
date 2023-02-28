b184c5a3e682a4e27d0afb49ef5b9657bbf602b7
originalmalek <originalmalek@gmail.com>
2023-02-28 19:23:46 -0300

    Add verbos names

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -2,11 +2,13 @@ from django.db import models
 
 
 class Pokemon(models.Model):
-	title = models.CharField(max_length=200)
-	title_en = models.CharField(max_length=200, blank=True)
-	title_jp = models.CharField(max_length=200, blank=True)
-	picture = models.ImageField(upload_to='pokemon_pics', null=True)
-	description = models.TextField(blank=True)
+	title = models.CharField(max_length=200, verbose_name='Название покемона')
+	title_en = models.CharField(max_length=200, blank=True, verbose_name='Название анг.')
+	title_jp = models.CharField(max_length=200, blank=True, verbose_name='Название яп.')
+	picture = models.ImageField(upload_to='pokemon_pics',
+	                            null=True,
+	                            verbose_name='Картинка')
+	description = models.TextField(blank=True, verbose_name='Описание')
 
 	previous_evolution = models.ForeignKey('self',
 	                                       verbose_name='Из кого эволюционирует',
@@ -22,14 +24,16 @@ class Pokemon(models.Model):
 class PokemonEntity(models.Model):
 	pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
 
-	lat = models.FloatField()
-	lon = models.FloatField()
+	lat = models.FloatField(verbose_name='Широта')
+	lon = models.FloatField(verbose_name='Долгота')
 
-	appeared_at = models.DateTimeField(null=True)
-	disappeared_at = models.DateTimeField(null=True, blank=True)
+	appeared_at = models.DateTimeField(null=True, verbose_name='Появился в')
+	disappeared_at = models.DateTimeField(null=True,
+	                                      blank=True,
+	                                      verbose_name='Исчез в')
 
-	level = models.IntegerField(default=0)
-	health = models.IntegerField(default=0)
-	strength = models.IntegerField(default=0)
-	defence = models.IntegerField(default=0)
-	stamina = models.IntegerField(default=0)
+	level = models.IntegerField(default=0, verbose_name='Уровень покемона')
+	health = models.IntegerField(default=0, verbose_name='Здоровье покемона')
+	strength = models.IntegerField(default=0, verbose_name='Сила покемона')
+	defence = models.IntegerField(default=0, verbose_name='Защита покемона')
+	stamina = models.IntegerField(default=0, verbose_name='Выносливость покемона')
